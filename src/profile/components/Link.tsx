fb4406b59a640a69c5f003e05c621d8adae3b4f5
Brandon Scott-Hill <bran.scotthill@gmail.com>
2022-04-03 18:14:35 +1200

    Fix link comp formatting

--- src/profile/components/Link.tsx
+++ src/profile/components/Link.tsx
@@ -2,19 +2,16 @@ import React from 'react';
 import './Link.css';
 
 interface Props {
-  href: string;
-  text: string;
+    href: string;
+    text: string;
 }
 
 export default class Link extends React.Component<Props> {
     render() {
         return (
-
             <li>
                 <a className='link' href={this.props.href}>{this.props.text}</a>
             </li>
-
-
         )
     }
 }
