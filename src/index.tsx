e19786396fa026a860ade2e1fb7e0863b44e726f
Brandon Scott-Hill <bran.scotthill@gmail.com>
2022-04-03 15:58:25 +1200

    Inital project structure

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import CV from './profile/CV';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CV />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ src/profile/CV.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './CV.less';
+
+export default class CV extends React.Component {
+    render() {
+        return (
+            <div className="wrapper">
+
+                <h1>hello</h1>
+
+            </div>
+        )
+    }
+}
