3c743d26082ff9a8c38e8328b1f84fc0f6246b30
Brandon Scott-Hill <bran.scotthill@gmail.com>
2022-04-06 17:06:07 +1200

    Add intro text

--- src/profile/components/Intro.tsx
+++ src/profile/components/Intro.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import './Intro.css';
 
 export default class Intro extends React.Component {
     render() {
         return (
             <div className='intro'>
-                Summarise your career here. You can make a PDF version of your resume using our free Sketch template
-                here. Donec quam felis, ultricies nec, pellentesque eu. Lorem ipsum dolor sit amet, consectetuer
-                adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
-                dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
-                quis, sem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.
-                Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit
-                amet nibh.
+                An experienced software engineer who uses creativity, past experiences and problem-solving techniques to
+                solve technical challenges. A passionate learner who is directly motivated by opportunities to grow and
+                learn. Well versed in writing code and DevOps practices and has had exposure to many different
+                programming languages and DevOps tools. Identified opportunities to better serve users' needs and have
+                created innovative solutions to exploit those opportunities. Enjoys being part of the team and
+                contributing to team culture.
+                <hr/>
             </div>
 
         )
